ce1e41d91b0d696379ac40d79e911052da457cd7
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-20 22:55:08 -0300

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,34 @@
+import statusCode from './statusCode.js'
+
+const btnSend = document.querySelector('#send')
+const select = document.querySelector('select')
+const Url = document.querySelector('#url')
+const newBtn = document.querySelector('#newBtn')
+
+
+btnSend.addEventListener('click',sendData)
+
+newBtn.addEventListener('click',()=>{
+    
+})
+
+
+async function sendData(e){
+
+    const method =select.value
+
+    if(Url.value === '')return e.preventDefault();
+    
+   const data = await fetch(Url.value,{
+        method,
+        
+    })
+    
+    const {status} = data
+    const StatusCode = statusCode[status]
+    const body = await data.json()
+    
+    const contetnType = data.headers.get('Content-Type')
+
+   
+}
