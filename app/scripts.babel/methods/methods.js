478849d22971e73e45e8d76af3969367acad55f0
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-26 23:36:57 -0300

    Create methods.js

---
+++ app/scripts.babel/methods/methods.js
@@ -0,0 +1,34 @@
+export default {
+ MethodPost :async function (){
+    try{
+    const response = await fetch(Url.value,{
+      method:'POST',
+      body,
+      headers
+  })
+  
+  const data =await response.json()
+  console.log(data)
+  
+  //console.log(GetDatas(response,data))
+    }catch(err){
+      throw err
+    }
+ },
+ MethodPost:async function (){
+  try{
+  const response = await fetch(Url.value,{
+    method:'POST',
+    body,
+    headers
+})
+
+const data =await response.json()
+console.log(data)
+
+//console.log(GetDatas(response,data))
+  }catch(err){
+    throw err
+  }
+}
+}
